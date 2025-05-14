927526a864b675f59607617257c6215f768b63e9
bandhan-majumder <bandhanmajumder16@gmail.com>
2025-05-14 21:20:58 +0530

    add admin specific page

---
+++ apps/web/app/admin/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Admin() {
+  return (
+    <div className='text-white text-center font-semibold text-3xl'>This is an admin page. Add new notion blog document ids to it</div>
+  )
+}
+
+export default Admin
