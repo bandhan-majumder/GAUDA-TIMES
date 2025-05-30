6fdc0506e81569ad9b16f06fe5ef4b5df768fe61
bandhan-majumder <bandhanmajumder16@gmail.com>
2025-05-30 23:42:07 +0530

    update nextconfig file for prisma

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,13 +1,17 @@
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
-  },
+    devIndicators: false,
+    experimental: {
+        authInterrupts: true,
+    },
+    webpack: (config, { isServer }) => {
+        if (isServer) {
+            config.plugins = [...config.plugins, new PrismaPlugin()];
+        }
+        return config;
+    },
 };
 
 export default nextConfig;
