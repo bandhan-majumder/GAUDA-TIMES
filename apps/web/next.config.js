10bfe4e16af9fb1f8690afc022c23296958f67ce
bandhan-majumder <bandhanmajumder16@gmail.com>
2025-06-15 20:31:24 +0530

    next config fix

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -6,6 +6,9 @@ const nextConfig = {
     experimental: {
         authInterrupts: true,
     },
+    images: {
+        domains: ["i.ibb.co","pexels.com","openai.com"],
+    },
     webpack: (config, { isServer }) => {
         if (isServer) {
             config.plugins = [...config.plugins, new PrismaPlugin()];
